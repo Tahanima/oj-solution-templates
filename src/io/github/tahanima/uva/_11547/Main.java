2bed644de4dd366cb825f770124da8b14b3d7085
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-21 22:21:41 +0600

    initial commit

---
+++ src/io/github/tahanima/uva/_11547/Main.java
@@ -0,0 +1,33 @@
+package io.github.tahanima.uva._11547;
+
+import java.util.Scanner;
+
+public class Main {
+    /**
+     * Returns the answers to the skill testing questions.
+     *
+     * @param number denotes the skill testing questions
+     */
+    public static int[] solve(int[] number) {
+        return new int[0];
+    }
+
+    /*
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        int testCases = scanner.nextInt();
+        int[] number = new int[testCases];
+
+        for (int cases = 0; cases < testCases; cases++)
+            number[cases] = scanner.nextInt();
+
+        int[] answer = solve(number);
+        for (int n: answer)
+            stringBuilder.append(String.format("%d%n", n));
+
+        System.out.print(stringBuilder);
+    }
+}
