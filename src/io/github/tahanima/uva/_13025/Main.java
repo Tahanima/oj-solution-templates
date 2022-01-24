3f3a8eee410a768d5623bfe5f44687f5716651be
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-01-24 14:21:43 +0600

    Add UVa 13025 solution

---
+++ src/io/github/tahanima/uva/_13025/Main.java
@@ -0,0 +1,20 @@
+package io.github.tahanima.uva._13025;
+
+public class Main {
+
+    /**
+     * @return a string containing the answer
+     */
+    public static String solve() {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        System.out.println(solve());
+    }
+}
