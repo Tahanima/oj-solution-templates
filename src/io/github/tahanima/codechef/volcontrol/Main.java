0e563acf0e03e4c891e7b63244f7f687de1d4357
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-05 21:10:25 +0600

    Add CodeChef VOLCONTROL template

---
+++ src/io/github/tahanima/codechef/volcontrol/Main.java
@@ -0,0 +1,43 @@
+package io.github.tahanima.codechef.volcontrol;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * @param x denotes the initial volumes of TVs
+     * @param y denotes the final volumes of TVs
+     * @return an integer arraylist containing the answers
+     */
+    public static ArrayList<Integer> solve(ArrayList<Integer> x, ArrayList<Integer> y) {
+        // Implement this method
+
+        return new ArrayList<>();
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        int cases = scanner.nextInt();
+
+        ArrayList<Integer> x = new ArrayList<>();
+        ArrayList<Integer> y = new ArrayList<>();
+
+        while (cases-- > 0) {
+            x.add(scanner.nextInt());
+            y.add(scanner.nextInt());
+        }
+
+        ArrayList<Integer> answers = solve(x, y);
+
+        for (int answer: answers) {
+            stringBuilder.append(String.format("%s%n", answer));
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
