b5f92d1d11bdc9210683ef301ea79806809a0240
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-05 20:59:25 +0600

    Add CodeChef EMAILREM template

---
+++ src/io/github/tahanima/codechef/emailrem/Main.java
@@ -0,0 +1,25 @@
+package io.github.tahanima.codechef.emailrem;
+
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * @param n denotes the total number of users
+     * @param u denotes the number of users who don't want to receive contest reminders
+     * @return an integer containing the answer
+     */
+    public static int solve(int n, int u) {
+        // Implement this method
+
+        return 0;
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(solve(scanner.nextInt(), scanner.nextInt()));
+    }
+}
