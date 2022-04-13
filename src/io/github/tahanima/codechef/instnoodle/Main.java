c7de72176f77c01d66b8b41c12289f69dbeee95b
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-14 01:03:45 +0600

    Add CodeChef INSTNOODLE solution template

---
+++ src/io/github/tahanima/codechef/instnoodle/Main.java
@@ -0,0 +1,28 @@
+package io.github.tahanima.codechef.instnoodle;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param x denotes the number of stoves
+     * @param y denotes the number of minutes available to serve customer orders
+     * @return an integer containing the answer
+     */
+    public static int solve(int x, int y) {
+        // Implement this method
+
+        return 0;
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(solve(scanner.nextInt(), scanner.nextInt()));
+    }
+}
