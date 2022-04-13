b051753657976599d9bdf9d5881760cf2f17af9b
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-14 01:23:14 +0600

    Add CodeChef CREDSCORE solution

---
+++ src/io/github/tahanima/codechef/credscore/Main.java
@@ -0,0 +1,24 @@
+package io.github.tahanima.codechef.credscore;
+
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * @param x denotes the present credit score
+     * @return a string containing the answer
+     */
+    public static String solve(int x) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(solve(scanner.nextInt()));
+    }
+}
