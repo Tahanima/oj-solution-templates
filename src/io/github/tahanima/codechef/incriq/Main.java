0cb2b552150ff13e9ab1d1c63f34350351ce42cd
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-15 13:47:56 +0600

    Add CodeChef INCRIQ solution template

---
+++ src/io/github/tahanima/codechef/incriq/Main.java
@@ -0,0 +1,27 @@
+package io.github.tahanima.codechef.incriq;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param x denotes the current iq of chef
+     * @return a string containing the answer
+     */
+    public static String solve(int x) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(solve(scanner.nextInt()));
+    }
+}
