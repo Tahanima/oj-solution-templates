8f6fe8967661e73626ff4fa21295835760ecaaa9
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-25 19:53:04 +0600

    Add beecrowd 1000 solution template

---
+++ src/io/github/tahanima/beecrowd/_1000/Main.java
@@ -0,0 +1,27 @@
+package io.github.tahanima.beecrowd._1000;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @return a string containing the answer
+     */
+    public static String solve() {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(solve());
+    }
+}
