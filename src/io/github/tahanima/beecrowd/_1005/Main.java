70290c401c3e6fba9cb8d8cb591063428465f024
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-27 23:24:05 +0600

    Add beecrowd 1005 solution template

---
+++ src/io/github/tahanima/beecrowd/_1005/Main.java
@@ -0,0 +1,29 @@
+package io.github.tahanima.beecrowd._1005;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param a denotes the first student's grade
+     * @param b denotes the second student's grade
+     * @return a string containing the answer
+     */
+    public static String solve(double a, double b) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(solve(scanner.nextDouble(), scanner.nextDouble()));
+    }
+}
