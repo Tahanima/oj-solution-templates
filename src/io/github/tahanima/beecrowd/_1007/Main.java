33349e03783eb12784747f12ed3303a7913ab5ad
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-27 23:36:33 +0600

    Add beecrowd 1007 solution template

---
+++ src/io/github/tahanima/beecrowd/_1007/Main.java
@@ -0,0 +1,31 @@
+package io.github.tahanima.beecrowd._1007;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param a denotes the first number
+     * @param b denotes the second number
+     * @param c denotes the third number
+     * @param d denotes the fourth number
+     * @return a string containing the answer
+     */
+    public static String solve(int a, int b, int c, int d) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(solve(scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
+    }
+}
