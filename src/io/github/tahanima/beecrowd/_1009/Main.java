44992fb8bd58266960790d86c1e416eac348df86
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-28 21:55:30 +0600

    Add beecrowd 1009 solution template

---
+++ src/io/github/tahanima/beecrowd/_1009/Main.java
@@ -0,0 +1,30 @@
+package io.github.tahanima.beecrowd._1009;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param name denotes the seller's name
+     * @param salary denotes the seller's fixed salary
+     * @param saleAmount denotes the seller's sale amount in a month
+     * @return a string containing the answer
+     */
+    public static String solve(String name, double salary, double saleAmount) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(solve(scanner.next(), scanner.nextDouble(), scanner.nextDouble()));
+    }
+}
