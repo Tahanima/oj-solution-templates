7821044115a102a2ac7dd39ba0223e960ffccb49
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-28 21:48:57 +0600

    Add beecrowd 1008 solution template

---
+++ src/io/github/tahanima/beecrowd/_1008/Main.java
@@ -0,0 +1,30 @@
+package io.github.tahanima.beecrowd._1008;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param num denotes the employee's number
+     * @param workedHours denotes the employee's worked hours in a month
+     * @param hourlyRate denotes the employee's hourly rate
+     * @return a string containing the answer
+     */
+    public static String solve(int num, int workedHours, double hourlyRate) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(solve(scanner.nextInt(), scanner.nextInt(), scanner.nextDouble()));
+    }
+}
