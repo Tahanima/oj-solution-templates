eb4479c5c11da048f35e6ab39a54635e77ff27ca
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-28 22:00:45 +0600

    Add beecrowd 1011 solution template

---
+++ src/io/github/tahanima/beecrowd/_1011/Main.java
@@ -0,0 +1,28 @@
+package io.github.tahanima.beecrowd._1011;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param r denotes the radius
+     * @return a string containing the answer
+     */
+    public static String solve(double r) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(solve(scanner.nextDouble()));
+    }
+}
