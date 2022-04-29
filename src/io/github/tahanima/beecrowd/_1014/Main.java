a5bcf5bdfab14be2a8d15a6a085ec78afc083afd
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-29 20:37:50 +0600

    Add beecrowd 1014 solution template

---
+++ src/io/github/tahanima/beecrowd/_1014/Main.java
@@ -0,0 +1,29 @@
+package io.github.tahanima.beecrowd._1014;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param x denotes the total distance
+     * @param y denotes the total fuel spent
+     * @return a string containing the answer
+     */
+    public static String solve(double x, double y) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(solve(scanner.nextDouble(), scanner.nextDouble()));
+    }
+}
