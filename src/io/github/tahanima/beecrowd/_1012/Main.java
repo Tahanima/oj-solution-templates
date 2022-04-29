3c18748f17228b012cb287dbdea4e3e89f0ba777
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-29 20:21:28 +0600

    Add beecrowd 1012 solution template

---
+++ src/io/github/tahanima/beecrowd/_1012/Main.java
@@ -0,0 +1,30 @@
+package io.github.tahanima.beecrowd._1012;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param a denotes the first floating point number
+     * @param b denotes the first floating point number
+     * @param c denotes the first floating point number
+     * @return a string containing the answer
+     */
+    public static String solve(double a, double b, double c) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(solve(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble()));
+    }
+}
