9a8b25513afa40ec4f54de3bd55a666406320d1b
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-30 15:20:52 +0600

    Add beecrowd 1017 solution template

---
+++ src/io/github/tahanima/beecrowd/_1017/Main.java
@@ -0,0 +1,29 @@
+package io.github.tahanima.beecrowd._1017;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param spentTime denotes the time spent in the trip
+     * @param avgSpeed denotes the average speed during the trip
+     * @return a string containing the answer
+     */
+    public static String solve(double spentTime, double avgSpeed) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(solve(scanner.nextDouble(), scanner.nextDouble()));
+    }
+}
