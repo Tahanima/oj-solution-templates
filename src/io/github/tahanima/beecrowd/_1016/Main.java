74561af75e6b29d3b784d8c3c1eaa68bc977e917
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-30 15:13:23 +0600

    Add beecrowd 1016 solution template

---
+++ src/io/github/tahanima/beecrowd/_1016/Main.java
@@ -0,0 +1,28 @@
+package io.github.tahanima.beecrowd._1016;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param distance denotes the distance covered by car Y
+     * @return a string containing the answer
+     */
+    public static String solve(int distance) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(solve(scanner.nextInt()));
+    }
+}
