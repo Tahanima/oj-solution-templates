3261365683358c64df67c87feac0a8e21ff11e73
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-30 20:43:47 +0600

    Add beecrowd 1020 solution template

---
+++ src/io/github/tahanima/beecrowd/_1020/Main.java
@@ -0,0 +1,28 @@
+package io.github.tahanima.beecrowd._1020;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param age denotes a person's age
+     * @return a string containing the answer
+     */
+    public static String solve(int age) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(solve(scanner.nextInt()));
+    }
+}
