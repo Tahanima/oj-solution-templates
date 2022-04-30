0f6b483833797b049f578b2854154918883647ec
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-04-30 15:06:10 +0600

    Add beecrowd 1015 solution template

---
+++ src/io/github/tahanima/beecrowd/_1015/Main.java
@@ -0,0 +1,31 @@
+package io.github.tahanima.beecrowd._1015;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param x1 denotes the x1 coordinate
+     * @param y1 denotes the y1 coordinate
+     * @param x2 denotes the x2 coordinate
+     * @param y2 denotes the y2 coordinate
+     * @return a string containing the answer
+     */
+    public static String solve(double x1, double y1, double x2, double y2) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(solve(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble()));
+    }
+}
