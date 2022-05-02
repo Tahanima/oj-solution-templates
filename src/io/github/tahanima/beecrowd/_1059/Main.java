51a0d0f0a6b5f7f5df8d45ffd6099c3b16779a6b
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-05-02 19:18:17 +0600

    Add beecrowd 1059 solution template

---
+++ src/io/github/tahanima/beecrowd/_1059/Main.java
@@ -0,0 +1,27 @@
+package io.github.tahanima.beecrowd._1059;
+
+import java.util.ArrayList;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @return an integer arraylist containing the answer
+     */
+    public static ArrayList<Integer> solve() {
+        // Implement this method
+
+        return new ArrayList<>();
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        for (int answer: solve()) {
+            System.out.println(answer);
+        }
+    }
+}
