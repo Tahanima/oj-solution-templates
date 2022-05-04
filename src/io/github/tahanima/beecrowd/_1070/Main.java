97ae858652e4bf7b8b67abcf31799cf48eeae67f
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-05-04 20:17:12 +0600

    Add beecrowd 1070 solution template

---
+++ src/io/github/tahanima/beecrowd/_1070/Main.java
@@ -0,0 +1,31 @@
+package io.github.tahanima.beecrowd._1070;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param x denotes a number
+     * @return an integer arraylist containing the answer
+     */
+    public static ArrayList<Integer> solve(int x) {
+        // Implement this method
+
+        return new ArrayList<>();
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int answer: solve(scanner.nextInt())) {
+            System.out.println(answer);
+        }
+    }
+}
