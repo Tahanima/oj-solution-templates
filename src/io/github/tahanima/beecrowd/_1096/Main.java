c70fb37a26b248b2bb9a56865f57bea30a48b1c1
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-05-04 20:42:07 +0600

    Add beecrowd 1096 solution template

---
+++ src/io/github/tahanima/beecrowd/_1096/Main.java
@@ -0,0 +1,23 @@
+package io.github.tahanima.beecrowd._1096;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @return a string containing the answer
+     */
+    public static String solve() {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        System.out.println(solve());
+    }
+}
