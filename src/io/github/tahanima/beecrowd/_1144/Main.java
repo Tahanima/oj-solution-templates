9982a4cb387b561c8976a1977c59581ad53b330d
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-05-05 18:17:12 +0600

    Add beecrowd 1144 solution template

---
+++ src/io/github/tahanima/beecrowd/_1144/Main.java
@@ -0,0 +1,28 @@
+package io.github.tahanima.beecrowd._1144;
+
+import java.util.Scanner;
+
+/**
+ * @author tahanima
+ */
+public class Main {
+
+    /**
+     * @param n denotes the number
+     * @return a string containing the answer
+     */
+    public static String solve(int n) {
+        // Implement this method
+
+        return "";
+    }
+
+    /**
+     * Takes care of the problem's input and output.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(solve(scanner.nextInt()));
+    }
+}
